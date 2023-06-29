ecf79a18b7987bd24faa8bdd5d3770a78f5d8d0f
taps99 <tpappsim@uoguelph.ca>
2023-06-29 16:57:53 -0400

    gui class w/ functions

--- gui_class.py
+++ gui_class.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import *
-from tkinter.ttk import *
+# from tkinter import *
+# from tkinter.ttk import *
 from tkinter import filedialog
 from tkinter import messagebox
 import pandas as pd
 import os
+import requests
+import concurrent.futures
+import re
+# import protein_seq
 
+
+
+# Create a class for the GUI instance
 class GUI:
+    # Initialize variables + window for interface
     def __init__(self, window):
         self.window = window
         self.processed_dfs = []
@@ -15,24 +23,28 @@ class GUI:
         self.output_directory = None
         self.file_listbox = None
         self.completion_label = None
-        self.output_listbox = None
+        # self.output_listbox = None
+        self.output_files_label = None
 
         # Setup widgets
         self.setup_widgets()
 
-
     def setup_widgets(self):
-        # window = tk.Tk()
         self.window.title("Non-Tryptic Peptide Extractor")
         self.window.geometry("800x600")
+        
 
-        # Style configuration
+        # Style configuration of widgets
         style = ttk.Style()
-        style.configure("TButton", padding=5, background="#ccc", font=('TkDefaultFont'))
-        style.configure("TListbox", padding=10, background="#eee")
-        style.configure('bold.TButton', padding=5, background="blue", font=('TkDefaultFont', 9,"bold"))
+        self.window.tk.call("source", "Azure-ttk-theme-main/azure.tcl")
+        self.window.tk.call("set_theme", "dark")
+       
+        style.configure("TButton", padding=5, font=('TkDefaultFont'))
+        style.configure("TListbox", padding=10)
+        style.configure('bold.TButton', padding=5, font=('TkDefaultFont', 9,"bold"), background = 'lightblue')
+        # style.configure('Heading.Label',)
 
-        # Widgets
+        # Define + arrange widgets in the interface
         frame1 = tk.Frame(self.window)
         frame1.grid(row = 2, column=0, sticky=tk.W)
 
@@ -47,10 +59,10 @@ class GUI:
         remove_button.grid(row=2, column=1, pady=10, padx=10, sticky=tk.W)
         # remove_button.pack(in_=top, side=LEFT)
 
-        process_button = ttk.Button(self.window, text="Process", command=self.process_files, style='bold.TButton')
+        process_button = ttk.Button(self.window, text="Process", command=self.process_files, style='Accent.TButton')
         process_button.grid(row=2, column=1, pady=10, padx=10, sticky=tk.E)
 
-        listbox_label = ttk.Label(self.window, text="List of selected PSM files:")
+        listbox_label = tk.Label(self.window, text="List of selected PSM files:")
         listbox_label.grid(row=0, column=0, pady=10, padx=10, sticky=tk.W)
         # listbox_label.pack(in_=top, side=LEFT)
 
@@ -58,54 +70,59 @@ class GUI:
         self.file_listbox.grid(row=1, column=0, columnspan=2, padx=10, sticky=tk.W)
         # file_listbox.pack(in_=top, side=TOP, expand=TRUE)
 
-        self.completion_label = Label(self.window, text=" ")
+        self.completion_label = tk.Label(self.window, text="")
         self.completion_label.grid(row=7, column=0, pady=10, padx=10, sticky=tk.W)
 
-        output_listbox_label = Label(self.window, text="Output files:")
+        self.output_files_label = tk.Label(self.window, text="")
+        self.output_files_label.grid(row=6, column=0, pady=10, padx=10, sticky=tk.W)
+
+        output_listbox_label = tk.Label(self.window, text="Output files:")
         output_listbox_label.grid(row=5, column=0, pady=10, padx=10, sticky=tk.W)
 
-        self.output_listbox = tk.Listbox(self.window, selectmode=tk.MULTIPLE, width=100, height=10)
-        self.output_listbox.grid(row=6, column=0, columnspan=2, padx=10, sticky=tk.W)
+        # self.output_listbox = tk.Listbox(self.window, selectmode=tk.MULTIPLE, width=100, height=10)
+        # self.output_listbox.grid(row=6, column=0, columnspan=2, padx=10, sticky=tk.W)
 
         output_directory_button = ttk.Button(frame2, text="Select Output Directory", command=self.select_output_directory)
         output_directory_button.grid(row=3, column=0, pady=10, padx=10, sticky=tk.W)
 
-        self.output_directory_label = Label(frame2, text="No output directory selected yet.")
+        self.output_directory_label = tk.Label(frame2, text="No output directory selected yet.")
         self.output_directory_label.grid(row=3, column=1, pady=10, padx=10, sticky=tk.W)
 
-
+    # Function to open file explorer to select files to be processed
     def browse_files(self):
         file_paths = filedialog.askopenfilenames(filetypes=[("TSV files", "*.tsv"), ("All files", "*.*")], multiple=True)
         for file_path in file_paths:
             self.file_listbox.insert(tk.END, file_path)
 
+    # Function to remove selected files from list
     def remove_files(self):
         selected_files = self.file_listbox.curselection()
         for index in selected_files[::-1]:
             self.file_listbox.delete(index)
 
+    # Function to select the directory that the output files should be output to
     def select_output_directory(self):
-        global output_directory
-        output_directory = filedialog.askdirectory()
-        self.output_directory_label["text"] = "Output directory: " + output_directory
+        # global output_directory
+        self.output_directory = filedialog.askdirectory()
+        self.output_directory_label["text"] = f"Output directory: {self.output_directory}"
         
-
+    # Function to setup interface elements (count of files, data structures to hold data,)
     def setup(self):
-        global processed_dfs
-        global processed_filepaths
-        processed_dfs = []
-        processed_filepaths = []
+        # global processed_dfs
+        # global processed_filepaths
+        self.processed_dfs = []
+        self.processed_filepaths = []
         count = 0
 
-        os.makedirs(output_directory, exist_ok=True)
+        os.makedirs(self.output_directory, exist_ok=True)
 
-        for i, listbox_entry in enumerate(self.file_listbox.get(0, END)):
+        for i, listbox_entry in enumerate(self.file_listbox.get(0, tk.END)):
             file_path = self.file_listbox.get(i)
             directory_name = os.path.basename(os.path.dirname(file_path))
             # file_name_without_ext = os.path.splitext(os.path.basename(file_path))[0]
             # output_file_name = f"{file_name_without_ext}_output_{directory_name}.tsv"
             sample_name = directory_name.split('.')[0]
-            processed_filepaths.append(sample_name)
+            self.processed_filepaths.append(sample_name)
             
             count += 1
 
@@ -115,17 +132,17 @@ class GUI:
             self.completion_label["text"] = f"Successfully processed {count} file."
         else:
             self.completion_label["text"] = f"Successfully processed {count} files."
-        return processed_dfs, processed_filepaths
+        return self.processed_dfs, self.processed_filepaths
 
 
     def process_data(self, processed_filepaths):
-        unprocessed_dfs = []
+        self.unprocessed_dfs = []
         count = 0
-        for i, listbox_entry in enumerate(self.file_listbox.get(0, END)):
+        for i, listbox_entry in enumerate(self.file_listbox.get(0, tk.END)):
             # Read the TSV file
             count += 1
             df = pd.read_csv(listbox_entry, delimiter='\t', low_memory=False)
-            unprocessed_dfs.append(df)
+            self.unprocessed_dfs.append(df)
 
             # Data transformations
             condition1 = ~(df['Peptide'].str.endswith('K') | df['Peptide'].str.endswith('R') & df['Prev AA'].isin(['K', 'R']))
@@ -144,8 +161,8 @@ class GUI:
             merged_df["Peptide:Protein"] = merged_df[['Peptide', 'Protein Description']].agg(':'.join, axis=1)
             merged_df_subset = merged_df[['Peptide:Protein', 'Tryptic State', 'Peptide', 'Prev AA', 'Next AA', 'Protein Description', 'Protein ID']]
             
-            processed_dfs.append(merged_df_subset)
-        return processed_dfs, unprocessed_dfs
+            self.processed_dfs.append(merged_df_subset)
+        return self.processed_dfs, self.unprocessed_dfs
 
 
     def output_files(self, processed_filepaths, processed_dfs, unprocessed_dfs):
@@ -155,10 +172,10 @@ class GUI:
         # print(processed_dict_peptide)
 
         # This is to get the file that contains ALL peptides from the input files (includes tryptic and non-tryptic)
-        combined_df = pd.concat(unprocessed_dfs, ignore_index=TRUE)
+        combined_df = pd.concat(unprocessed_dfs, ignore_index=True)
         combined_df["Peptide:Protein"] = combined_df[['Peptide', 'Protein Description']].agg(':'.join, axis=1)
         # combined_df.to_csv('./all_peptides.csv', index=False)
-        combined_df.to_csv(os.path.join(output_directory, 'all_peptides.csv'), index=False)
+        combined_df.to_csv(os.path.join(self.output_directory, 'all_peptides.csv'), index=False)
 
 
         # This is to get the unique proteins from the entire dataset (includes tryptic and non-tryptic)
@@ -169,7 +186,7 @@ class GUI:
                 combined_df_subset[sample_name] = combined_df_subset['Protein ID'].isin(df['Protein ID']).astype(int)
 
         # combined_df_subset.to_csv('all_unique_proteins.csv', index=False)
-        combined_df_subset.to_csv(os.path.join(output_directory, 'all_unique_proteins.csv'), index=False)
+        combined_df_subset.to_csv(os.path.join(self.output_directory, 'all_unique_proteins.csv'), index=False)
         # result_df = pd.DataFrame(columns=['Peptide:Protein'])
 
 
@@ -199,7 +216,7 @@ class GUI:
         
         # File that contains all non-tryptic peptides across samples
         # master_peptide_df.to_csv('./non_tryptic_peptides.csv', index=False)
-        master_peptide_df.to_csv(os.path.join(output_directory, 'non_tryptic_peptides.csv'), index=False)
+        # master_peptide_df.to_csv(os.path.join(self.output_directory, 'non_tryptic_peptides.csv'), index=False)
 
 
 
@@ -207,31 +224,141 @@ class GUI:
         # Drop duplicates based on protein column to get only one row per protein, presence still based on peptide associated with this protein
         proteins_df = master_peptide_df.drop_duplicates(subset=['Protein ID'])
         # proteins_df.to_csv('./non_tryptic_proteins.csv', index=False)
-        proteins_df.to_csv(os.path.join(output_directory, 'non_tryptic_proteins.csv'), index=False)
+        proteins_df.to_csv(os.path.join(self.output_directory, 'non_tryptic_proteins.csv'), index=False)
+
+        return master_peptide_df
+
+        
 
 
-        output_filepaths = ['./all_peptides.csv', './all_unique_proteins.csv', './non_tryptic_peptides.csv', './protein_output.csv']
-        for filepath in output_filepaths:
-            df = pd.read_csv(filepath, low_memory=False)
-            peptide_count = df['Peptide'].nunique() # Modify this line if your column name differs
-            self.output_listbox.insert(tk.END, f'File: {filepath}, Number of peptides: {peptide_count}')
 
-        return processed_dfs
+        # output_filepaths = [f'{self.output_directory}/all_peptides.csv', f'{self.output_directory}/all_unique_proteins.csv', f'{self.output_directory}/non_tryptic_peptides.csv', f'{self.output_directory}/non_tryptic_proteins.csv']
+        # for filepath in output_filepaths:
+        #     df = pd.read_csv(filepath, low_memory=False)
+        #     peptide_count = df['Peptide'].nunique() # Modify this line if your column name differs
+        #     self.output_listbox.insert(tk.END, f'File: {filepath}, Number of peptides: {peptide_count}')
+
+    def fetch_sequence(self, peptide):
+        url = f"https://www.uniprot.org/uniprot/{peptide}.fasta" 
+        response = requests.get(url)
+        # Check if successful request and extract protein sequence 
+        if response.ok:
+            lines = response.text.strip().split("\n")
+            protein_sequence = "".join(lines[1:])
+            return peptide, protein_sequence
+        else:
+            return peptide, None
+    
+    def get_protein_sequence(self, peptide_df):
+        # peptide_df = pd.read_csv(f'{self.output_directory}/non_tryptic_proteins.csv')
+        peptides_id = peptide_df['Protein ID'].to_list()
+        # Can alter this value to set the number of worker threads for parallel execution 
+        max_workers = 100
+
+        # Store protein sequences with respective protein IDs in dictionary
+        protein_sequences = {}
+
+        # Use ThreadPoolExecutor from concurrent.futures to launch parallel fetching of protein sequences
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            # List to store Future objects which represent the execution of the fetch_sequence method
+            futures = []
+            for peptide in peptides_id:
+                # Submit fetch_sequence method for execution and store the Future object and then add it to the futures list
+                future = executor.submit(self.fetch_sequence, peptide)
+                futures.append(future)
+
+            # Iterate over completed Future objects
+            for future in concurrent.futures.as_completed(futures):
+                # Extract the result of the fetch_sequence method from the Future object using the .result method
+                peptide, sequence = future.result()
+                # Add sequence to respective protein ID in dictionary
+                if sequence:
+                    protein_sequences[peptide] = sequence
+                else:
+                    protein_sequences[peptide] = 'NaN'
+
+        # Create Protein Sequence column in df and output to file
+        peptide_df['Protein Sequence'] = [protein_sequences[peptide] for peptide in peptides_id]
+        peptide_df.to_csv(f'{self.output_directory}/nontryp_pept_sequences.csv', index=False)
+        # print(peptide_df.columns)
+        return peptide_df
 
 
+    def peptide_seq_match(self, df):
+        print(df)
+        print(type(df))
+        # match peptide with protein sequence using re 
+        peptide = str(df['Peptide'])
+        prev_AA = str(df['Prev AA'])
+        tryp_state = str(df['Tryptic State'])
+        sequence = str(df['Protein Sequence'])
+        matches = re.finditer(peptide, sequence)
+        print(matches)
+
+        n_terminal_list = []
+        c_terminal_list = []
+
+        for match in matches:
+            # Gives start and end indices that you can use to get the amino acids before and after the match in a given sequence
+            start_index = match.start()
+            end_index = match.end()
+
+            # Calculate indices for extracting the 5 amino acids before and after the match
+            # 2nd argument acts as a boundary, so there's no values that go outside of the sequence length that can't be indexed (can't go below 0, can't go above length of sequence)
+            before_start_index = max(start_index - 5, 0)
+            after_end_index = min(end_index + 5, len(sequence))
+
+            # Extract 5 amino acids before and after the match
+            before_match = sequence[before_start_index:start_index]
+            after_match = sequence[end_index:after_end_index]
+
+            n_terminal = before_match + peptide[:5]
+            c_terminal = peptide[-5:] + after_match
+            
+            if tryp_state == 'Non-Tryptic':
+                n_terminal_list.append(n_terminal)
+                c_terminal_list.append(c_terminal)
+            else:
+                if prev_AA == 'K' or prev_AA == 'R':
+                    c_terminal_list.append(c_terminal)
+                elif peptide.endswith('K') or peptide.endswith('R'):
+                    n_terminal_list.append(n_terminal)
+        # Add dashes to the end of sequences that are less than 10 aa long for N-terminal cleavages
+        for i in range(len(n_terminal_list)):
+            string = n_terminal_list[i]
+            while len(string) < 10:
+                string = "-" + string
+            n_terminal_list[i] = string
+        # Add dashes to beginning of sequences that are less than 10 aa long for C-terminal cleavages
+        for i in range(len(c_terminal_list)):
+            string = c_terminal_list[i]
+            while len(string) < 10:
+                string += "-"
+            c_terminal_list[i] = string
+                
+        n_terminal_str = ''.join(n_terminal_list)
+        c_terminal_str = ''.join(c_terminal_list)
+
+        terminal_series = pd.Series({'N-terminal cleavage': n_terminal_str, 'C-terminal cleavage': c_terminal_str})
+        print(n_terminal_list)
+        # print(terminal_series)
+        return terminal_series
+    
+
+    # This gets executed when you click 'Process' button
     def process_files(self):
         self.processed_dfs, self.processed_filepaths = self.setup()
         self.processed_dfs, unprocessed_dfs = self.process_data(self.processed_filepaths)
-        self.output_files(processed_filepaths, processed_dfs, unprocessed_dfs)
-
+        self.peptide_seq_match(self.get_protein_sequence(self.output_files(self.processed_filepaths, self.processed_dfs, unprocessed_dfs)))
+        # self.output_files(self.processed_filepaths, self.processed_dfs, unprocessed_dfs))
 
 
 def main():
-    # Initialize window
+    # Initialize new window, create instance of GUI class
     window = tk.Tk()
     gui = GUI(window)
     window.mainloop()
 
-
+# executes main() if script is being run directly, doesn't execute if script is imported
 if __name__ == "__main__":
     main()
